96ee10cfa20f99a2bb730f719a4e4548d13cbc5d
Dominik Meister <dominik.s.meister@gmail.com>
2024-08-05 16:31:02 +0200

    added all navigation tabs

--- app/page.tsx
+++ app/page.tsx
@@ -103,8 +103,35 @@ export default function Home() {
                             >
                                 About
                             </a>
-                            <a className="flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1">
-                                About
+                            <a
+                                href="#projects"
+                                className={`flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1 ${
+                                    activeSection === "projects"
+                                        ? "text-blue-500 font-bold"
+                                        : ""
+                                }`}
+                            >
+                                Projects
+                            </a>
+                            <a
+                                href="#hobbies"
+                                className={`flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1 ${
+                                    activeSection === "hobbies"
+                                        ? "text-blue-500 font-bold"
+                                        : ""
+                                }`}
+                            >
+                                Hobbies
+                            </a>
+                            <a
+                                href="#contact"
+                                className={`flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1 ${
+                                    activeSection === "contact"
+                                        ? "text-blue-500 font-bold"
+                                        : ""
+                                }`}
+                            >
+                                Contact
                             </a>
                             <a className="flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1">
                                 Documents
@@ -116,7 +143,7 @@ export default function Home() {
                         <div className="relative flex items-center justify-center">
                             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
                                 <h1 className="text-4xl text-white">
-                                    Dominik Meister
+                                    Greetings! I&apos;m Dominik Meister
                                 </h1>
                             </div>
                         </div>
@@ -128,6 +155,33 @@ export default function Home() {
                 <span id="about"></span>
                 <section className="about-me flex flex-col justify-center p-6 z-[1]">
                     <h2 className="text-3xl mb-4">about me.</h2>
+                    <p className="max-w-5xl">
+                        Hello! I am Dominik, a passionate developer with a love
+                        for creating innovative solutions. I enjoy working on
+                        web development projects and learning new technologies.
+                    </p>
+                </section>
+                <span id="projects"></span>
+                <section className="about-me flex flex-col justify-center p-6 z-[1]">
+                    <h2 className="text-3xl mb-4">Projects</h2>
+                    <p className="max-w-5xl">
+                        Hello! I am Dominik, a passionate developer with a love
+                        for creating innovative solutions. I enjoy working on
+                        web development projects and learning new technologies.
+                    </p>
+                </section>
+                <span id="hobbies"></span>
+                <section className="about-me flex flex-col justify-center p-6 z-[1]">
+                    <h2 className="text-3xl mb-4">Hobbies</h2>
+                    <p className="max-w-5xl">
+                        Hello! I am Dominik, a passionate developer with a love
+                        for creating innovative solutions. I enjoy working on
+                        web development projects and learning new technologies.
+                    </p>
+                </section>
+                <span id="contact"></span>
+                <section className="about-me flex flex-col justify-center p-6 z-[1]">
+                    <h2 className="text-3xl mb-4">Contact</h2>
                     <p className="max-w-5xl">
                         Hello! I am Dominik, a passionate developer with a love
                         for creating innovative solutions. I enjoy working on
