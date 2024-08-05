2391e9afd02566caec043b969daab7c0f2497b18
Dominik Meister <dominik.s.meister@gmail.com>
2024-08-05 12:18:03 +0200

    fixed overlay issue

--- app/page.tsx
+++ app/page.tsx
@@ -56,11 +56,18 @@ export default function Home() {
                 <div className="flex min-h-screen flex-col items-center justify-between">
                     <div className="flex flex-col items-center justify-center flex-grow">
                         <div className="relative flex items-center justify-center">
-                            <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-                                <h1 className="text-4xl">
-                                    Greetings! I&apos;m Dominik Meister
+                            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+                                <h1 className="text-4xl text-white">
+                                    Dominik Meister
                                 </h1>
                             </div>
+                            {/* <div className="relative">
+                                <div id="title-bg-1" className=""></div>
+                                <div id="title-bg-2"></div>
+                                <h1 className="text-4xl text-white">
+                                    Greetings! I'm Dominik Meister
+                                </h1>
+                            </div> */}
                         </div>
                     </div>
                     <a href="#about">
@@ -68,7 +75,7 @@ export default function Home() {
                     </a>
                 </div>
                 <span id="about"></span>
-                <section className="about-me flex flex-col justify-center p-6">
+                <section className="about-me flex flex-col justify-center p-6 z-[1]">
                     <h2 className="text-3xl mb-4">About Me</h2>
                     <p className="max-w-5xl">
                         Hello! I am Dominik, a passionate developer with a love
