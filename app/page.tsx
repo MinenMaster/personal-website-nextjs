68f2e763873db83e483241f14bc2d4f663f25c24
Dominik Meister <dominik.s.meister@gmail.com>
2024-08-05 16:04:51 +0200

    added navigation

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
     const [data, setData] = useState("Loading...");
+    const [activeSection, setActiveSection] = useState("");
 
     const URL =
         "https://personal-website-nodejs-serverless-function-express.vercel.app/api";
@@ -25,6 +26,28 @@ export default function Home() {
         getHello()
             .then((data) => setData(data.message))
             .catch((error) => console.error(error));
+    }, []);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll("section");
+            let currentSection = "";
+
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                if (window.scrollY >= sectionTop - 60) {
+                    currentSection = section.getAttribute("id");
+                }
+            });
+
+            setActiveSection(currentSection);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
 
     return (
@@ -52,8 +75,43 @@ export default function Home() {
                     }
                 ></div>
             </div>
+
             <main className="flex flex-col items-center">
                 <div className="flex min-h-screen flex-col items-center justify-between">
+                    <span id="home"></span>
+                    <nav className="flex justify-around p-4 m-4 backdrop-blur-md fixed top-0 z-10 border border-gray-600 rounded-full shadow-md max-w-screen-lg mx-auto mt-4">
+                        {/* Links Section */}
+                        <div className="items-center lg:flex ">
+                            <a
+                                href="#home"
+                                className={`flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1 ${
+                                    activeSection === "home"
+                                        ? "text-blue-500 font-bold"
+                                        : ""
+                                }`}
+                            >
+                                Home
+                            </a>
+
+                            <a
+                                href="#about"
+                                className={`flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1 ${
+                                    activeSection === "about"
+                                        ? "text-blue-500 font-bold"
+                                        : ""
+                                }`}
+                            >
+                                About
+                            </a>
+                            <a className="flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1">
+                                About
+                            </a>
+                            <a className="flex text-white hover:text-blue-500 cursor-pointer transition-colors duration-300 mx-1">
+                                Documents
+                            </a>
+                        </div>
+                    </nav>
+
                     <div className="flex flex-col items-center justify-center flex-grow">
                         <div className="relative flex items-center justify-center">
                             <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
@@ -69,7 +127,7 @@ export default function Home() {
                 </div>
                 <span id="about"></span>
                 <section className="about-me flex flex-col justify-center p-6 z-[1]">
-                    <h2 className="text-3xl mb-4">About Me</h2>
+                    <h2 className="text-3xl mb-4">about me.</h2>
                     <p className="max-w-5xl">
                         Hello! I am Dominik, a passionate developer with a love
                         for creating innovative solutions. I enjoy working on
