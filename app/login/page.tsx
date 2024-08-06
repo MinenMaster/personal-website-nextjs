7929f27f39da7c4fddd7e3855d5a1e1be0ffb3e4
Dominik Meister <dominik.s.meister@gmail.com>
2024-08-06 21:40:29 +0200

    fixed type error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLogin = async (e) => {
+    const handleLogin = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
         const res = await fetch("/api/login", {
             method: "POST",
