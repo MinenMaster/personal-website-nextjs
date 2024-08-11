c4e3eb0ffe230b92a1eaa17873d50b2a0e89a257
Dominik Meister <dominik.s.meister@gmail.com>
2024-08-11 21:17:38 +0200

    added keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
@@ -19,6 +18,10 @@ export default function RootLayout({
                 <meta
                     name="description"
                     content="My personal portfolio website"
+                />
+                <meta
+                    name="keywords"
+                    content="Dominik Meister, dominikmeister, portfolio, developer, software engineer, full stack, frontend, backend, web, website, app, application, programming, coding, development, design, ux, ui, user experience, user interface, react, next.js, typescript, javascript, python, java, html5, css3, node.js, express, amazonaws, postgresql, vercel, docker, git, github, gitlab, visualstudiocode, mysql"
                 />
             </head>
             <body className={inter.className}>
