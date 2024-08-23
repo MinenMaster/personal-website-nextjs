1b4a4ff9ba57de413fcea56fcb48fc9814d58548
Dominik Meister <dominik.s.meister@gmail.com>
2024-08-24 01:09:10 +0200

    fixed body width

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                ></meta>
                 <title>Portfolio | Dominik Meister</title>
                 <meta
                     name="description"
