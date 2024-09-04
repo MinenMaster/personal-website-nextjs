b749b87c93fd0de221340bd272d12ff4f98da49c
Dominik Meister <dominik.s.meister@gmail.com>
2024-09-04 22:30:56 +0200

    removed previous adding

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -76,43 +76,23 @@ const Footer = () => {
             </div>
 
             <Modal isOpen={isModalOpen} onClose={closeModal}>
-                <h2 className="text-3xl mb-4 text-white">Legal Notice</h2>
-                <p className="text-white mb-4">
+                <h2 className="text-3xl mb-4">Legal Notice</h2>
+                <p className="mb-4">
                     This website and its content are licensed under the MIT
                     License. All rights reserved. Please ensure to adhere to the
                     terms of the license for any reuse or distribution.
                 </p>
-                <h3 className="text-2xl mb-3 text-white">License:</h3>
                 <p>
-                    MIT License
-                    <br />
-                    <br />
-                    Copyright (c) 2024 Dominik Meister
-                    <br />
-                    <br />
-                    Permission is hereby granted, free of charge, to any person
-                    obtaining a copy of this software and associated
-                    documentation files (the "Software"), to deal in the
-                    Software without restriction, including without limitation
-                    the rights to use, copy, modify, merge, publish, distribute,
-                    sublicense, and/or sell copies of the Software, and to
-                    permit persons to whom the Software is furnished to do so,
-                    subject to the following conditions:
-                    <br />
-                    <br />
-                    The above copyright notice and this permission notice shall
-                    be included in all copies or substantial portions of the
-                    Software.
-                    <br />
-                    <br />
-                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-                    KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-                    WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-                    PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-                    OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-                    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-                    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-                    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+                    For more details, you can view the full license{" "}
+                    <a
+                        href="https://github.com/MinenMaster/personal-website-nextjs/blob/main/LICENSE.md"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 underline"
+                    >
+                        here
+                    </a>
+                    .
                 </p>
             </Modal>
         </footer>
