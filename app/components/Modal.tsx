2d7ab2b9ff0976567fa0b9c1df6123d38e7493e3
Dominik Meister <dominik.s.meister@gmail.com>
2026-04-10 09:17:43 +0200

    fix: adjust Modal background opacity and add backdrop blur effect

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -26,14 +26,16 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
     return (
         <div
-            className={`fixed w-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 transition-opacity duration-200 ${
+            className={`fixed w-screen inset-0 bg-black bg-opacity-40 backdrop-blur-sm flex items-center justify-center z-50 transition-opacity duration-200 ${
                 isVisible ? "opacity-100" : "opacity-0"
             }`}
+            onClick={close}
         >
             <div
                 className={`bg-gray-900 bg-opacity-30 backdrop-blur-md p-6 border border-gray-600 rounded-2xl shadow-lg max-w-3xl w-full mx-4 relative transform transition-transform duration-200 ${
                     isVisible ? "scale-100" : "scale-95"
                 }`}
+                onClick={(e) => e.stopPropagation()}
             >
                 <button
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 transition text-3xl font-bold p-2"
